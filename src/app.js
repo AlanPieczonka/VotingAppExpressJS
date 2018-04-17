5d13b3053a6b228e28b9bc24dbc62605178b945c
Alan Pieczonka <alanpieczonka@gmail.com>
2018-04-18 00:10:06 +0200

    Configure dotenv in app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 import routes from './routes';
 
+dotenv.config();
 const app = express();
 app.disable('x-powered-by');
 app.set('view engine', 'html');
