2a50a5710accbdd67893b1b76190ba17f6bd4ed9
Alan Pieczonka <alanpieczonka@gmail.com>
2018-04-20 08:59:32 +0200

    Add avatarURL field and isValidAvatarURL method to User model

--- src/models/User.js
+++ src/models/User.js
@@ -10,10 +10,16 @@ const schema = new mongoose.Schema({
     lowercase: true,
   },
   passwordHash: { type: String, required: true },
+  avatarURL: { type: String, min: 1, max: 120 },
 });
 
 schema.methods.isValidPassword = function isValidPassword(password) {
   return bcrypt.compareSync(password, this.passwordHash);
+};
+
+schema.methods.isValidAvatarURL = function isValidAvatarURL(url) {
+  const regexp = /([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png))/i;
+  return regexp.test(url) || url === '';
 };
 
 schema.methods.setPassword = function setPassword(password) {
