eceb0004b0503534cb829deed3bc5e0dcac2043e
Alan Pieczonka <alanpieczonka@gmail.com>
2018-04-27 12:06:53 +0200

    Add basic User /join validation

--- src/routes/join.js
+++ src/routes/join.js
@@ -1,10 +1,20 @@
 import express from 'express';
+import { check, validationResult } from 'express-validator/check';
 import User from './../models/User';
 import parseErrors from '../utils/parseErrors';
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', [
+  check('user.email').isEmail().withMessage('must be an email'),
+  check('user.password').isLength({ min: 6 }).withMessage('must be at least 6 characters long'),
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.mapped() });
+  }
+
+
   const { email, password } = req.body.user;
   const user = new User({ email });
   user.setPassword(password);
