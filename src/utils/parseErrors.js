798737890b36244ccf4e5a923adafaf48306bd3b
Alan Pieczonka <alanpieczonka@gmail.com>
2018-05-08 20:23:01 +0200

    Use arrow syntax in utils/parseErrors.js

--- src/utils/parseErrors.js
+++ src/utils/parseErrors.js
@@ -1,9 +1,9 @@
 import _ from 'lodash';
 
-export default function (errors) {
+export default (errors) => {
   const result = {};
   _.forEach(errors, (val, key) => {
     result[key] = val.message;
   });
   return result;
-}
+};
