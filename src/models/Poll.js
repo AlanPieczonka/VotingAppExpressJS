67af5d79c1bf0418fbbc3b99449b7d4d3a3712c7
Alan Pieczonka <alanpieczonka@gmail.com>
2018-05-13 22:47:34 +0200

    Create Poll model

---
+++ src/models/Poll.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const schema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    options: [
+      {
+        title: {
+          type: String,
+          required: true,
+        },
+        votes: {
+          default: 0,
+          type: Number,
+        },
+      },
+    ],
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model('Poll', schema);
