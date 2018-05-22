2e04b41ea261935a692d391bc0dedcdf836eda7a
Alan Pieczonka <alanpieczonka@gmail.com>
2018-05-22 23:03:44 +0200

    Adapt authenticate.js middleware to Postman testing

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -2,12 +2,15 @@ import jwt from 'jsonwebtoken';
 import User from '../models/User';
 
 export default (req, res, next) => {
-  const header = req.headers.authorization;
+  const header = req.headers.authorization,
+        postmanTesting = false;
   let token;
+  if (postmanTesting) {
+    token = header;
+  } else {
     if (header) token = header.split(' ')[1];
-    // console.log(token);
-  // token = header; // just for the POSTMAN testing
-  console.log(token);
+  } 
+
   if (token) {
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       if (err) {
