a07e2d2597d178b3fb4098656106f9a0b0aef7d5
Alan Pieczonka <alanpieczonka@gmail.com>
2018-05-23 14:52:50 +0200

    Move signup functionality from /join to /user route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,38 @@
 import express from 'express';
+import { validationResult } from 'express-validator/check';
+import User from './../models/User';
+import parseErrors from '../utils/parseErrors';
+import { validateEmail, validatePassword } from '../utils/validate';
 import Poll from './../models/Poll';
 import authenticate from '../middlewares/authenticate';
 
 const router = express.Router();
+
+router.post('/', [
+  validateEmail('user.email'),
+  validatePassword('user.password'),
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.mapped() });
+  }
+
+  const { email, password } = req.body.user;
+  const user = new User({ email });
+  user.setPassword(password);
+  user
+    .save()
+    .then((userRecord) => {
+      res.json({ user: userRecord.toAuthJSON() });
+    })
+    .catch((err) => {
+      const message = Object.values(parseErrors(err.errors))[0];
+      if (message) {
+        return res.status(400).json({ error: { message } });
+      }
+      return res.status(503).json({ error: { message: 'There has been an error with saving data to the database' } });
+    });
+});
 
 router.get('/polls', authenticate, (req, res) => {
   Poll.find({ userId: req.currentUser._id })
