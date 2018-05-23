cdaedd5213d166e2830ad070a576d4ad16f241e7
Alan Pieczonka <alanpieczonka@gmail.com>
2018-05-23 10:18:45 +0200

    Delete '/users' index route (no need for it at the moment)

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,14 +5,6 @@ import authenticate from '../middlewares/authenticate';
 const router = express.Router();
 router.use(authenticate);
 
-// GET current user's profile properties * ONLY AUTHENTICATED USER *
-router.get('/', (req, res) =>
-  res.json({
-    user: {
-      email: req.currentUser.email,
-    },
-  }));
-
 // GET specific current user's polls * ONLY AUTHENTICATED USER *
 router.get('/polls', (req, res) => {
   Poll.find({ userId: req.currentUser._id })
