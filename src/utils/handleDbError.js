41723e1e01f3db17590f076741ba510875d36be4
Alan Pieczonka <alanpieczonka@gmail.com>
2018-05-23 21:00:54 +0200

    Improve handleDbError util

--- src/utils/handleDbError.js
+++ src/utils/handleDbError.js
@@ -1,9 +1,9 @@
 import parseErrors from '../utils/parseErrors';
 
 export default (err, res) => {
-  const message = Object.values(parseErrors(err.errors))[0];
+  const message = Object.values(parseErrors(err.errors))[0] || err.message;
   if (message) {
-    return res.status(400).json({ error: { message } });
+    return res.status(404).json({ error: { message } });
   }
   return res.status(503).json({ error: { message: 'There has been an error with the database' } });
 };
