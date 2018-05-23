d4d90a296bf044899aa895538b74c351a02e33f0
Alan Pieczonka <alanpieczonka@gmail.com>
2018-05-23 10:20:16 +0200

    Use authenticate middleware in user route explicitly

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,10 +3,9 @@ import Poll from './../models/Poll';
 import authenticate from '../middlewares/authenticate';
 
 const router = express.Router();
-router.use(authenticate);
 
 // GET specific current user's polls * ONLY AUTHENTICATED USER *
-router.get('/polls', (req, res) => {
+router.get('/polls', authenticate, (req, res) => {
   Poll.find({ userId: req.currentUser._id })
     .then(polls => res.json({ polls }))
     .catch(err => res.json({ error: "You don't have any polls my friend!" }));
